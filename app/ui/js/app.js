79eab2f702a7ef42a0bde0d0d1ca8fff41c38aaf
crazycreak <github@felberbauer.com>
2016-05-13 21:34:45 +0200

    add sample bootstrap button

--- app/ui/js/app.js
+++ app/ui/js/app.js
@@ -1,8 +1,31 @@
+var BootstrapButton = React.createClass({
+	render: function() {
+		return (
+			<a {...this.props}
+			    href="javascript:;"
+			    role="button"
+			    className={(this.props.className || '') + ' btn'} />
+		);
+	}
+});
+
 var App = React.createClass({
-        render: function() {
-                var message = 'Hello World Application!';
-                return <h1>{message}</h1>;
-        }
+	render: function() {
+		var message = 'Hello World Application!'
+		var header = null;
+
+		header = (
+			<h1>{message}</h1>
+		);
+
+		return (
+			<div className="main">
+				{header}
+				<BootstrapButton className="btn-default">Button 1</BootstrapButton>
+				<BootstrapButton className="btn-default">Button 2</BootstrapButton>
+			</div>
+		);
+	}
 });
 
 // render app
