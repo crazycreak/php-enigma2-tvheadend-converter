57e7d0e9c6e9eb9f8c524be3c31fce6275cfecd9
crazycreak <github@felberbauer.com>
2016-05-14 20:58:32 +0200

    [WIP] add Enigma2 Components

--- app/ui/js/app.js
+++ app/ui/js/app.js
@@ -9,18 +9,75 @@ var BootstrapButton = React.createClass({
 	}
 });
 
+var Enigma2BouquetItem = React.createClass({
+	render: function() {
+		return (
+			<div className="Enigma2BouquetItem">{this.props.text}</div>
+		);
+	}
+});
+
+var Enigma2BouquetList = React.createClass({
+	render: function() {
+		var bouquetItems = this.props.data.map(function(item) {
+			return (
+				<Enigma2BouquetItem text={item.e2servicename} />
+			);
+		});
+		return (
+			<div className="Enigma2BouquetList">
+				{bouquetItems}
+			</div>
+		);
+	}
+});
+
+var Enigma2Box = React.createClass({
+	loadBouquetsFromServer: function() {
+		var path = '/enigma2';
+		var bouquetsUrl = this.props.url + path + '/bouquets/tv';
+		$.ajax({
+			url: bouquetsUrl,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				if (data['code'] == 200) {
+					this.setState({data: data['data']});
+				}
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(bouquetsUrl, status, err.toString());
+			}.bind(this)
+		});
+	},
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		this.loadBouquetsFromServer();
+	},
+	render: function() {
+		return (
+			<div className="Enigma2Box">
+				<Enigma2BouquetList data={this.state.data} />
+			</div>
+		);
+	}
+});
+
 var App = React.createClass({
 	render: function() {
-		var message = 'Hello World Application!'
-		var header = null;
+		var enigma2BoxHeaderMessage = 'Enigma2 Box!';
+		var enigma2BoxHeader = null;
 
-		header = (
-			<h1>{message}</h1>
+		enigma2BoxHeader = (
+			<h1>{enigma2BoxHeaderMessage}</h1>
 		);
 
 		return (
-			<div className="main">
-				{header}
+			<div className="mainApp">
+				{enigma2BoxHeader}
+				<Enigma2Box url="/api/v1" />
 				<BootstrapButton className="btn-default">Button 1</BootstrapButton>
 				<BootstrapButton className="btn-default">Button 2</BootstrapButton>
 			</div>
