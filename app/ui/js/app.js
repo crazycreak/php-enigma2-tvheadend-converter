55c064a499c5198fce91bddcc7bcf4bdd7058844
crazycreak <github@felberbauer.com>
2016-05-26 23:12:13 +0200

    [WIP] update ui, add channel number

--- app/ui/js/app.js
+++ app/ui/js/app.js
@@ -12,11 +12,20 @@ var BootstrapButton = React.createClass({
 var Enigma2ChannelItem = React.createClass({
 	render: function() {
 		var servicename = this.props.data.e2servicename;
+		if (typeof(this.props.data.e2servicename) != 'undefined') {
+			servicename = <span className="service-name">{this.props.data.e2servicename}</span>;
+		}
+
+		var number = null;
+		if (typeof(this.props.data.number) != 'undefined') {
+			number = <span className="number label label-default">{this.props.data.number}</span>;
+		}
 
 		return (
 			<li className="channel-item list-group-item">
 				<BootstrapButton className="btn-info btn-xs">map</BootstrapButton>
-				<span className="service-name">{servicename}</span>
+				{number}
+				{servicename}
 			</li>
 		);
 	}
